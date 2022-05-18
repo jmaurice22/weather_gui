633db89c42110a41721d467abcb2694561e12ef3
jmaurice22 <57735087+jmaurice22@users.noreply.github.com>
2022-05-18 16:00:48 -0700

    Update app.py
    added new weather conditions and frame background color

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import tkinter as tk
 from tkinter import *
 import datetime as dt
 from PIL import ImageTk, Image
+import geocoder
 import requests
-
+import re
 
 # Method to request server for weather data
 def get_weather():
@@ -25,42 +26,67 @@ def get_weather():
         temp = int(temp)
         weather = data['weather']
         description = weather[0]['description']
-        temp_label = tk.Label(data_frame, text=f"{temp}{degree_sign}F", font=font_tuple)
+        temp_label = tk.Label(data_frame, text=f"{temp}{degree_sign}F", font=font_tuple, bg='#a8d5e5')
         temp_label.grid(row=2, column=0, sticky='nsew')
-        city_label = tk.Label(data_frame, text=city.upper(), font=('calibre', 10, 'bold'))
+        city_label = tk.Label(data_frame, text=city.upper(), font=('calibre', 10, 'bold'), bg='#a8d5e5')
         city_label.grid(row=1, column=1)
-        description_label = tk.Label(data_frame, text=description, font=('calibre', 10, 'bold'))
+        description_label = tk.Label(data_frame, text=description, font=('calibre', 10, 'bold'), bg='#a8d5e5')
         description_label.grid(row=2, column=2)
         entry.delete(0, END)
         show_image(description)
     else:
         # ERROR HANDLING
-        temp_label = tk.Label(data_frame, text="Not Found", font=font_tuple)
+        temp_label = tk.Label(data_frame, text="Not Found", font=font_tuple, bg='#a8d5e5')
         temp_label.grid(row=2, column=0, sticky='nsew')
-        city_label = tk.Label(data_frame, text="", font=('calibre', 10, 'bold'))
+        city_label = tk.Label(data_frame, text="", font=('calibre', 10, 'bold'), bg='#a8d5e5')
         city_label.grid(row=1, column=1)
         entry.delete(0, END)
 
-
+# Method to display corresponding images
 def show_image(description):
     if description == "clear sky":
-        image_label = tk.Label(data_frame, image=sunny)
+        image_label = tk.Label(data_frame, image=sunny, bg='#a8d5e5')
         image_label.grid(row=2, column=1)
-    elif description == "broken clouds":
-        image_label = tk.Label(data_frame, image=broken_clouds)
+    elif description == "broken clouds" or description == "few clouds" or description == "scattered clouds" \
+            or description == "broken clouds":
+        image_label = tk.Label(data_frame, image=broken_clouds, bg='#a8d5e5')
         image_label.grid(row=2, column=1)
     elif description == "snow":
-        image_label = tk.Label(data_frame, image=snow)
+        image_label = tk.Label(data_frame, image=snow, bg='#a8d5e5')
         image_label.grid(row=2, column=1)
-    elif description == "rain":
-        image_label = tk.Label(data_frame, image=rain)
+    elif description == "rain" or description == "shower rain":
+        image_label = tk.Label(data_frame, image=rain, bg='#a8d5e5')
         image_label.grid(row=2, column=1)
+
+# Method to get Geolocation lat & long coordinates
+def geolocation():
+    degree_sign = u"\N{DEGREE SIGN}"
+    g = geocoder.ip('me')
+    latitude = g.lat
+    longitude = g.lng
+    try:
+        # API KEY
+        api_key = 'f0d6ab958d9c3097d6dcbaf7427c4ec4'
+        res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=imperial&appid={api_key}").json()
+        main = res['main']
+        temp = main['temp']
+        temp = int(temp)
+        weather = res['weather']
+        description = weather[0]['description']
+        temp_label = tk.Label(data_frame, text=f"{temp}{degree_sign}F", font=font_tuple, bg='#a8d5e5')
+        temp_label.grid(row=2, column=0, sticky='nsew')
+        description_label = tk.Label(data_frame, text=description, font=('calibre', 10, 'bold'), bg='#a8d5e5')
+        description_label.grid(row=2, column=2)
+        show_image(description)
+    except:
+        pass
 
 # create a window
 window = tk.Tk()
 window.title("Weather")
 window.geometry('450x300')
 window.iconbitmap('app_icon.ico')
+
 
 # Date variables
 date = dt.datetime.now()
@@ -73,9 +99,7 @@ header_frame.pack(fill=tk.BOTH)
 title = tk.Label(header_frame, text="Forecast", fg="White", bg='#4f524f', font=('calibre', 18, 'bold'))
 title.pack()
 
-# declaring string variable
-# for storing city value
-# city = tk.StringVar()
+
 
 # Images for different weather conditions
 # image size is 64px
@@ -85,16 +109,17 @@ snow = ImageTk.PhotoImage(Image.open("D:\Python_Projects\weather_gui\images\snow
 rain = ImageTk.PhotoImage(Image.open("D:\Python_Projects\weather_gui\images\\rain.png"))
 
 # frame for the entry and json response
-frame = tk.Frame(window)
+frame = tk.Frame(window, bg='#a8d5e5')
 frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-search_frame = tk.Frame(frame)
+search_frame = tk.Frame(frame, bg='#a8d5e5')
 search_frame.pack(pady=3)
 search_frame.rowconfigure(0)
 search_frame.columnconfigure([0, 1, 2], minsize=50)
 
+
 # label for entry field
-entry_label = tk.Label(search_frame, text="Search", font=('calibre', 10, 'bold'), fg='#1bd90d')
+entry_label = tk.Label(search_frame, text="Search", font=('calibre', 10, 'bold'), bg='#a8d5e5')
 entry_label.grid(row=0, column=0)
 
 # entry for the city search
@@ -107,13 +132,15 @@ button = tk.Button(search_frame, text='Enter', bg="#21cf2d", fg="white", relief=
 button.grid(row=0, column=2)
 
 # frame to display temperature and icon
-data_frame = tk.Frame(frame)
+data_frame = tk.Frame(frame,  bg='#a8d5e5')
 data_frame.rowconfigure([0, 1, 2], minsize=50)
 data_frame.columnconfigure([0, 1, 2, 3], minsize=100)
-data_frame.pack(padx=1)
+data_frame.pack(padx=1, fill=tk.BOTH, expand=True)
 font_tuple = ("Comic Sans MS", 24, "bold")
-date = tk.Label(data_frame, text=format_date, font=('Comic Sans MS', 10, 'bold'))
+date = tk.Label(data_frame, text=format_date, font=('Comic Sans MS', 10, 'bold'), bg='#a8d5e5')
 date.grid(row=0, column=0)
 
 # Run the application
-window.mainloop()
+if __name__ == '__main__':
+    geolocation()
+    window.mainloop()
